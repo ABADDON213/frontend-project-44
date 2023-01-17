0bad6276a2782c377f3bb4e81c2f03059f24737a
ABADDON21 <luchinkirill@yandex.ru>
2023-01-17 13:33:09 +0300

    4 stage

--- .github/workflows/src/cli.js
+++ .github/workflows/src/cli.js
@@ -1,10 +1,10 @@
 import readlineSync from 'readline-sync';
 
 const welcome = (text) => {
-    console.log ('Welcome to the Brain Games!');
-    const userName = readlineSync.question('May I have your name? ');
-    console.log (`Hello, ${userName}!`);
-    if (text !== undefined) console.log(text);
-    return userName;
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  if (text !== undefined) console.log(text);
+  return userName;
 };
-export default welcome
+export default welcome;
